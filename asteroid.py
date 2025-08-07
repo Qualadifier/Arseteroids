668e31b215850784b127be194b1a86437f17e29e
Qualadifier <qualadifier@gmail.com>
2025-08-06 19:16:50 -0500

    B:Added ship and env build

---
+++ asteroid.py
@@ -0,0 +1,10 @@
+from CircleShape import *
+
+
+def Asteroid():
+    
+def __init__(self, x, y, radius):
+    for circle in CircleShape
+        pygame.draw.circle(x, y, 2)
+        circle.update(self.velocity * dt)
+
--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+from asteroidfield import *
+
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -10,13 +13,34 @@ print ("Starting Asteroids!")
 print (f"Screen width: {SCREEN_WIDTH}")
 print (f"Screen height: {SCREEN_HEIGHT}")
 
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updatable, drawable)
+
+asteroids = pygame.sprite.Group()
+Asteroid.containers = (asteroids, updatable, drawable)
+
+AsteroidField.containers = (updatable)
+
+ship = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT / 2)
+
+clock = pygame.time.Clock()
+dt=0
+
 def main():
 	while True:	
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+		dt = clock.tick(60) / 1000
+		updatable.update(dt)
 		screen.fill((0,0,0))
+		for sprite in drawable:
+			sprite.draw(screen)
+
 		pygame.display.flip()
+		
+
 
 if __name__ == "__main__":
     main()
